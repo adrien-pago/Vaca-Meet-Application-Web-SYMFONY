e1cfc97fac747a0a4f950570d528f5735daf0b73
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-06-07 16:32:06 +0200

    up

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,12 +17,14 @@ class SecurityController extends AbstractController
 {
     
 
+
 /**
  * @Route("/login", name="app_login")
 */
 public function login(Request $request, AuthenticationUtils $authenticationUtils, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
 {
 
+   
    
     $error = $authenticationUtils->getLastAuthenticationError();
     $lastUsername = $authenticationUtils->getLastUsername();
