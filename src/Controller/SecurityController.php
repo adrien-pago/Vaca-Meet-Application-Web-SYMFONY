2a137d6da8c636a470e5de3b0d1d3d4fdc6bcec9
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-06-07 08:54:00 +0200

    up controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -22,42 +22,28 @@ class SecurityController extends AbstractController
     /**
  * @Route("/login", name="app_login")
  */
-public function login(Request $request, AuthenticationUtils $authenticationUtils, UserPasswordHasherInterface $passwordHasher): Response
+public function login(Request $request, AuthenticationUtils $authenticationUtils, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
 {
-    // Récupérer les erreurs de connexion, le dernier email utilisé, etc.
     $error = $authenticationUtils->getLastAuthenticationError();
     $lastUsername = $authenticationUtils->getLastUsername();
 
-    // Créer le formulaire de connexion en passant le dernier email utilisé
     $loginForm = $this->createForm(LoginFormType::class, ['email' => $lastUsername]);
-
-    // Gérer la soumission du formulaire
     $loginForm->handleRequest($request);
 
-    // Si le formulaire est soumis et valide
     if ($loginForm->isSubmitted() && $loginForm->isValid()) {
-        // Récupérer les données soumises
         $formData = $loginForm->getData();
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $formData['email']]);
 
-        // Rechercher l'utilisateur dans la base de données par son email
-        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $formData['email']]);
-
-
-        // Vérifier si l'utilisateur existe et si le mot de passe est correct
         if (!$user || !$passwordHasher->isPasswordValid($user, $formData['password'])) {
-            // Si l'utilisateur n'existe pas ou si le mot de passe est incorrect, rediriger avec un message d'erreur
             $this->addFlash('error', 'Identifiants invalides.');
             return $this->redirectToRoute('app_login');
         }
 
-        // Authentifier l'utilisateur
-        // (vous pouvez ajouter ici toute logique d'authentification supplémentaire nécessaire)
+        // Ici, tu peux ajouter la logique d'authentification supplémentaire si nécessaire
 
-        // Redirection vers la page d'accueil
         return $this->redirectToRoute('home');
     }
 
-    // Rendre la vue avec le formulaire de connexion et les éventuelles erreurs
     return $this->render('security/login.html.twig', [
         'loginForm' => $loginForm->createView(),
         'last_username' => $lastUsername,
