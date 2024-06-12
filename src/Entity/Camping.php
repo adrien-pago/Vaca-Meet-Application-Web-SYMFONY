c237a6482cfcc9b61cf68ed5da16ce553c1a54f4
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-06-12 16:06:18 +0200

    up

--- src/Entity/Camping.php
+++ src/Entity/Camping.php
@@ -13,8 +13,8 @@ class Camping implements PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id()
-     * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
