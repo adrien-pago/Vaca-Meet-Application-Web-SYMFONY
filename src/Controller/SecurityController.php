1b61e07cbe8991949edaeb716b5224589b93b7a8
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-06-13 11:15:33 +0200

    up

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,8 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
 
 class SecurityController extends AbstractController
 {
@@ -32,6 +34,12 @@ class SecurityController extends AbstractController
             'loginForm' => $form->createView(),
             'error' => $error,
         ]);
+    }
+    
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        // Redirect to the homepage after successful login
+        return new RedirectResponse($this->generateUrl('home'));
     }
 
     /**
