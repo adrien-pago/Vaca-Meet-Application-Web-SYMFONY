ee02ee3669d4e5810426a9dfae33b2a94e5c8796
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-07-15 14:02:47 +0200

    up

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+// première page quand on arrive sur le site internet
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        return $this->render('security/login.html.twig');
+    }
+}
