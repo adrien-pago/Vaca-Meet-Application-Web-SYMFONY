3b6babd94f449b4838a934722ec3f961090d415f
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-07-16 14:24:17 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,13 +2,6 @@
 
 use App\Kernel;
 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=Vaca_Meet', 'vaca_meet', 'A?n5wj399');
-    echo "Connexion réussie !";
-} catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-}
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
